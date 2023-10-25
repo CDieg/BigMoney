ea157c89dc98355727c10a88cb1374423d0502fa
Carlos Dieguez <info@carlosdieguez.com>
2023-10-25 22:47:12 +0200

    Intro day 1

---
+++ BigMoney/Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+
+
+    // Start is called before the first frame update
+    void Awakw()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        // Tell the player motor to move using the value from our movement action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
