8c2c258f0d45524e5479c7abdf02a295adb3515a
Carlos Dieguez <info@carlosdieguez.com>
2023-11-07 01:03:04 +0100

    Assets

---
+++ BigMoney/Assets/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform hitTransform = collision.transform;
+        if (hitTransform.CompareTag("Player"))
+        {
+            Debug.Log("HitPlayer");
+            hitTransform.GetComponent<PlayerHealth>().TakeDamage(1);
+        }
+
+        Destroy(gameObject);
+    }
+}
